b77482acc5b3594a421eb3a873141033828f26d9
Shahrear Ahmed <shahrearahmedshuvon003@gmail.com>
2021-10-13 13:25:00 +0600

    second

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { createContext } from 'react';
 
-const AuthProvider = () => {
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    //const  {children}=props;
+
     return (
         <div>
-            
+            <AuthContext.Provider>
+                {children}
+            </AuthContext.Provider>
         </div>
     );
 };
